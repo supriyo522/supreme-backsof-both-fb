7b6daf006650a4a4f0aab68c126e946b68715778
supriyo522 <134998523+supriyo522@users.noreply.github.com>
2025-10-09 00:12:08 +0530

    Update GetInTouch.jsx

--- components/GetInTouch.jsx
+++ components/GetInTouch.jsx
@@ -42,7 +42,7 @@ export default function GetInTouch() {
 
     setIsSubmitting(true);
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact`, {
+      const response = await fetch(`https://supreme-backsof-both-fb.vercel.app/api/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
